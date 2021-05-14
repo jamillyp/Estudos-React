7b850e2c9ef4817c63b1331944a05bfa623a9656
Jamilly Pinheiro Lima <jamilly.pinheiro@imobsoft.com.br>
2021-05-14 15:55:48 -0300

    criando componentes com react

---
+++ formacao-react-alura/ceep/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormularioCadastro from './components/FormularioCadastro';
+import ListaNotas from './components/ListaNotas';
+function App() {
+  return (
+    <>
+    <FormularioCadastro />
+    <ListaNotas />
+    </>
+  );
+}
+
+export default App;
